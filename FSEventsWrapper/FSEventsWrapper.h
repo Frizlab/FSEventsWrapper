f220e48b520674736121744016c60585bf3b1ec9
Frizlab <fload@me.com>
2018-07-06 11:52:45 +0200

    Initial Commit

---
+++ FSEventsWrapper/FSEventsWrapper.h
@@ -0,0 +1,19 @@
+//
+//  FSEventsWrapper.h
+//  FSEventsWrapper
+//
+//  Created by François Lamboley on 06/07/2018.
+//  Copyright © 2018 Frizlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FSEventsWrapper.
+FOUNDATION_EXPORT double FSEventsWrapperVersionNumber;
+
+//! Project version string for FSEventsWrapper.
+FOUNDATION_EXPORT const unsigned char FSEventsWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSEventsWrapper/PublicHeader.h>
+
+
