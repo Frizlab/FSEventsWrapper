aa389c696f1aabb264ffdca73fa6f326914f0b01
Frizlab <fload@me.com>
2018-07-09 13:18:21 +0200

    Create a “direct link” test to check if the “mark own” option works from there

---
+++ FSEventsWrapperDirectLinkTest/FSEventsWrapperDirectLinkTest-BridgingHeader.h
@@ -0,0 +1,16 @@
+/*
+ * FSEventsWrapperDirectLinkTest-BridgingHeader.h
+ * FSEventsWrapper
+ *
+ * Created by François Lamboley on 09/07/2018.
+ * Copyright © 2018 Frizlab. All rights reserved.
+ */
+
+#ifndef FSEventsWrapperDirectLinkTest_BridgingHeader_h
+# define FSEventsWrapperDirectLinkTest_BridgingHeader_h
+
+# include "CFSEventUserInfo.h"
+# include "FSEventsWrapperPrivate.h"
+# include "FSEventStreamCallbackHandler.h"
+
+#endif /* FSEventsWrapperDirectLinkTest_BridgingHeader_h */
---
+++ FSEventsWrapperDirectLinkTest/main.swift
@@ -0,0 +1,37 @@
+/*
+ * main.swift
+ * FSEventsWrapperDirectLinkTest
+ *
+ * Created by François Lamboley on 09/07/2018.
+ * Copyright © 2018 Frizlab. All rights reserved.
+ */
+
+import Foundation
+
+
+/* This target has been created to check whether the “mark self” options would
+ * work when the wrapper is linked directly and not from a Framework. Alas, it
+ * does not… */
+
+
+class CallbackHandler : NSObject, FSEventStreamCallbackHandler {
+	
+	func fsChanged(inFolder folderPath: String, eventId: FSEventStreamEventId, becauseOfUs isEventFromUs: Bool) {
+		NSLog("%@ - %@", String(isEventFromUs), folderPath)
+	}
+	
+}
+
+
+let callbackHandler = CallbackHandler()
+let w = FSEventsWrapper(path: "/Users/frizlab/Downloads", fsEventStreamFlags: FSEventStreamCreateFlags(kFSEventStreamCreateFlagMarkSelf), callbackHandler: callbackHandler)
+w.startWatching()
+
+Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false){ t in
+	let f = fopen("/Users/frizlab/Downloads/FSEventsWrapperDirectLinkTest.\(Int.random(in: 0..<250)).test", "w+")
+	fclose(f)
+}
+
+repeat {
+	RunLoop.main.run(mode: .default, before: Date(timeIntervalSinceNow: 0.1))
+} while true
